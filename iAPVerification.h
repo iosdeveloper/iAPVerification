6f222677e8407eee13a1e3156272e9e20397f8f8
iosdeveloper <max.iphone.dev@gmail.com>
2012-01-11 19:10:42 +0100

    Find binaries used for cracking

--- iAPVerification.h
+++ iAPVerification.h
@@ -1,9 +1,8 @@
 //
 //  iAPVerification.h
-//  iap
 //
 //  Created by Max Bäumle.
-//  Copyright (c) 2011 Max Bäumle. All rights reserved.
+//  Copyright (c) 2011-2012 Max Bäumle. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
@@ -20,11 +19,10 @@ typedef enum {
     NSMutableData *_receivedData;
 }
 
-- (id)init:(SKPaymentTransaction *)paymentTransaction isSandbox:(BOOL)sandbox delegate:(id)delegate;
-+ (id)verifyPurchase:(SKPaymentTransaction *)paymentTransaction isSandbox:(BOOL)sandbox delegate:(id)delegate;
++ (void)verifyPurchase:(SKPaymentTransaction *)paymentTransaction isSandbox:(BOOL)sandbox delegate:(id)delegate;
++ (void)verifyPurchase:(SKPaymentTransaction *)paymentTransaction serverUrl:(NSString *)urlString isSandbox:(BOOL)sandbox delegate:(id)delegate;
 
-- (id)init:(SKPaymentTransaction *)paymentTransaction serverUrl:(NSString *)urlString isSandbox:(BOOL)sandbox delegate:(id)delegate;
-+ (id)verifyPurchase:(SKPaymentTransaction *)paymentTransaction serverUrl:(NSString *)urlString isSandbox:(BOOL)sandbox delegate:(id)delegate;
++ (BOOL)findBinaries; // Find binaries used for cracking on the device
 
 @end
 
@@ -32,7 +30,7 @@ typedef enum {
 
 @optional
 
-- (void)verificationFailed:(int)errorCode error:(NSError *)error;
+- (void)verificationFailed:(iAPErrorCode)errorCode error:(NSError *)error;
 - (void)purchaseVerified:(NSDictionary *)dictionary; // Available keys: 'quantity', 'product_id', 'transaction_id', 'purchase_date', 'app_item_id', 'bid', 'bvrs'
 
 @end
